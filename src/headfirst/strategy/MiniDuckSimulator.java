d6fea47b740ab16e8ac17d24447e4f77b0991346
Andrew Tran <tranandrew@gmail.com>
2023-08-23 14:19:38 -0500

    Duck example for intro Code to Interface, not Implementation

---
+++ src/headfirst/strategy/MiniDuckSimulator.java
@@ -0,0 +1,25 @@
+package headfirst.strategy;
+
+public class MiniDuckSimulator {
+ 
+	public static void main(String[] args) {
+
+		Duck mallard = new MallardDuck();
+		mallard.performQuack();
+		mallard.performFly();
+ 
+//		MallardDuck	mallard = new MallardDuck();
+//		RubberDuck	rubberDuckie = new RubberDuck();
+//		DecoyDuck	decoy = new DecoyDuck();
+//
+//		ModelDuck	model = new ModelDuck();
+//
+//		mallard.performQuack();
+//		rubberDuckie.performQuack();
+//		decoy.performQuack();
+//
+//		model.performFly();
+//		model.setFlyBehavior(new FlyRocketPowered());
+//		model.performFly();
+	}
+}
